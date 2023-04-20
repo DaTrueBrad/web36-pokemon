71e5205cb5ddac210bacdd316c0037af0393eed6
Brady Bott <bradybott@Bradys-MBP.localdomain>
2023-04-20 09:57:53 -0600

    halfway

---
+++ src/screens/DexScreen.jsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import PokedexCard from '../components/PokedexCard'
+
+const DexScreen = () => {
+  const [pokemon, setPokemon] = useState([])
+  const [search, setSearch] = useState("")
+
+  const handleSearch = (e) => setSearch(e.target.value.toLowerCase())
+
+  useEffect(() => {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon?limit=1000')
+      .then((res) => {
+        let newState = res.data.results.map((item, index) => {
+          item.img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+          return item
+        })
+        setPokemon(newState)
+        console.log(newState)
+      })
+  }, [])
+
+  const pokemonDisplay = pokemon
+  .filter((poke, index) => {
+    return poke.name.includes(search)
+  })
+  .map((poke, index) => {
+    return <PokedexCard poke={poke} index={index}/>
+  })
+
+  return (
+    <div>
+      <h1>Pokedex</h1>
+      <input type="text" placeholder='Search' onChange={handleSearch}/>
+      {pokemonDisplay}
+    </div>
+  )
+}
+
+export default DexScreen
